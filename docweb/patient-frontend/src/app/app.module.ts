57f0cd9eedd5b4d5b44b9c56ff7b5e4334d071ba
Harshit Jain <59441001+harshit-jainn@users.noreply.github.com>
2020-10-23 11:37:16 +0530

    Add files via upload

---
+++ docweb/patient-frontend/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PatientModalComponent } from './components/modal/patient-modal/patient-modal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    PatientModalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
